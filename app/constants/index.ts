20341bcf6808800483121b0b3f4f68d5310540fe
mikkel <mikkelelvers@outlook.com>
2024-11-12 11:17:55 +0100

    Added Menu

---
+++ app/constants/index.ts
@@ -0,0 +1,61 @@
+export const menu = [
+    {
+        category: "Burgers",
+        subtitle: "Alle burgere serveres med salat, tomat, rødløg, syltet agurk og pommes frites.",
+        items: [
+            { name: "Big Ben", description: "Bestående af cheddar ost og spejlæg", price: 169 },
+            { name: "El Toro", description: "Bestående af cheddar ost, paprika og jalapeños", price: 169 },
+            { name: "Den Lille Havfrue", description: "Bestående af cheddar ost", price: 169 },
+            { name: "Statue Of Liberty", description: "Bestående af cheddar ost og chilistegte champignon", price: 169 },
+            { name: "Istanbul", description: "Bestående af cheddar ost, tyrkisk pastrami og sucuk", price: 169 },
+        ]
+    },
+    {
+        category: "Sandwiches",
+        subtitle: "Alle sandwiches serveres med grovfritter og salat.",
+        items: [
+            { name: "Kyllingsandwich", description: "Serveres med humus og creme fraiche dressing", price: 159 },
+            { name: "Clubsandwich", description: "Med kylling og bacon, serveres med guacamole og karrydressing", price: 159 },
+            { name: "Sandwich (vegetarisk)", description: "Serveres med creme fraiche dresssing, humus og tzatziki", price: 159 },
+            { name: "Sandwich med røget laks", description: "Serveres med tzatziki", price: 159 },
+            { name: "Sandwich med tunmousse", description: "Serveres med citron", price: 159 },
+            { name: "Ribeye Sandwich", description: "Serveres med grillet ribeye, pesto, løgmarmelade og tzakiki", price: 179 },
+        ]
+    },
+    {
+        category: "Fra Grillen",
+        items: [
+            { name: "Ribeye Steak (3000g)", description: "Serveres med stegte kartofler og garniture | vælg mellem bearnaise-, eller pebersauce", price: 320 },
+            { name: "Hakkebøf (250g. hakket øksekød)", description: "Serveres med stegte kartofler og garniture | vælg mellem bearnaise-, eller pebersauce", price: 210 },
+            { name: "Kyllingeret", description: "Mørt kyllingebryst, serveres med pommes frites, humus og garniture", price: 189 },
+        ]
+    },
+    {
+        category: "Mexicansk",
+        items: [
+            { name: "Enchilada (kylling, oksekød)", description: "Sammenrullet ovnbagt tortilla fyldt med kød og smelteost, som serveres med salsa, creme fraiche, guacamole og salat", price: 169 },
+            { name: "Nachos", description: "Serveres med gucamole, salsa, creme fraiche, oliven, jalapeños og ost", price: 159 },
+            { name: "Nachos med kylling", description: "Serveres med guacamole, salsa, creme fraiche, oliven, jalapeños og ost", price: 169 },
+        ]
+    },
+    {
+        category: "Pizza",
+        subtitle: "Vælg mellem hvidløgs, chili eller basilikumolie. Ingredienser på pizza kan ikke fravælges.",
+        items: [
+            { name: "Four Italian Cheeses Pizza", description: "Buffal mozzarella dop, gorgonzola ost, stracchino ost, fortina ost, basilikum, italiensk ekstra jomfru olivenolie", price: 147 },
+            { name: "Vegetarian Pizza", description: "Mozzarella fiordilatte, grillet courglette, grillet aubergine, tomat, basilikum, italiensk ekstra jomfru olivenolie", price: 147 },
+            { name: "Bufala Pizza", description: "Buffal mozzarella dop, tomatsauce, basilikum, italiensk ekstra jomfru olivenolie", price: 147 },
+            { name: "Pizza Istanbul", description: "Ost, stracchino ost, fontina ost og tyrkisk sucuk", price: 147 },
+        ]
+    },
+    {
+        category: "Børnemenu for børn under 12 år",
+        items: [
+            { name: "Chicken Dippers", description: "Serveres med pommes frites og ketchup", price: 99 },
+            { name: "Børne pasta m. kylling", price: 99 },
+            { name: "Børneburger", description: "Serveres med pommes frites og ketchup", price: 99 },
+            { name: "Pandekage m. kylling", price: 99 },
+            { name: "Fiskefilet", description: "Serveres med pommes frites og remoulade", price: 99 },
+        ]
+    }
+]

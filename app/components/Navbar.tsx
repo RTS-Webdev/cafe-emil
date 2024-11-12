c085e0cffba613bd5f9752cfbf9c03dc0a574380
mikkel <mikkelelvers@outlook.com>
2024-11-12 11:17:27 +0100

    Removed Bars Icon

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,24 +1,13 @@
-import { HiOutlineBars3 as Bars } from "react-icons/hi2";
 import Link from "next/link";
 
 export default function Navbar() {
-    return (
-        <header className="w-full fixed top-0 z-50">
-            <nav className="flex items-center justify-between px-10 py-6 relative text-white">
-                <button 
-                    className="text-4xl" 
-                    aria-label="Toggle navigation menu"
-                >
-                    <Bars />
-                </button>
-
-                <Link 
-                    href="/" 
-                    className="text-3xl font-[family-name:var(--font-family)]"
-                >
-                    Café Emil
-                </Link>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="w-full h-0 top-0 z-50">
+      <nav className="flex items-center justify-between px-10 py-6 relative bg-white text-black">
+        <Link href="/" className="text-3xl font-playball">
+          Café Emil
+        </Link>
+      </nav>
+    </header>
+  );
 }

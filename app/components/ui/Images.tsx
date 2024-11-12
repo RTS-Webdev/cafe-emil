d891b0de319e91fb36587320b03c179a72edd196
mikkel <mikkelelvers@outlook.com>
2024-11-12 11:17:08 +0100

    Added images

---
+++ app/components/ui/Images.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+const imageSources = [
+  { src: "/img.svg", alt: "Reserver bord" },
+  { src: "/img.svg", alt: "Åbningstider" },
+  { src: "/img.svg", alt: "Bestil et bord" },
+];
+
+export default function Images() {
+  return (
+    <section className="mt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {imageSources.map((img, index) => (
+        <figure key={index} className="aspect-square overflow-hidden">
+          <Image
+            src={img.src}
+            alt={img.alt}
+            width={600}
+            height={1000}
+            priority={true}
+            sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+            className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
+          />
+        </figure>
+      ))}
+    </section>
+  );
+}

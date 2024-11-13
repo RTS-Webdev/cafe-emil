98e4b8d8c21073b42a1fef8d38b52f8e64999f68
Mikkel Nothlev <mikkel@Mikkels-MacBook-Pro.local>
2024-11-13 13:04:11 +0100

    Added navlinks

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,35 @@
+'use client'
+
 import Link from "next/link";
 
 export default function Navbar() {
+  const handleScroll = (sectionId: string) => {
+    setTimeout(() => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, 100);
+  };
+
   return (
-    <header className="w-full h-0 top-0 z-50">
-      <nav className="flex items-center justify-between px-10 py-6 relative bg-white text-black">
+    <header className="w-full h-0 top-0">
+      <nav className="flex items-center justify-between px-10 py-10 relative bg-white text-black">
         <Link href="/" className="text-3xl font-playball">
           Café Emil
         </Link>
+        
+        <div className="flex gap-6">
+          <a onClick={() => handleScroll('menu')} className="hover:text-gray-600 scroll-smooth">
+            Menu
+          </a>
+          <a onClick={() => handleScroll('bordbestilling')} className="hover:text-gray-600 scroll-smooth">
+            Bordbestilling
+          </a>
+          <a onClick={() => handleScroll('findus')} className="hover:text-gray-600 scroll-smooth">
+            Find os
+          </a>
+        </div>
       </nav>
     </header>
   );

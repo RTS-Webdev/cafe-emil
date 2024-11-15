7df4bb3ffae0c5fe26893738380b197c5d0a6e78
Mikkel Nothlev <mikkelelvers@outlook.com>
2024-11-15 08:25:54 +0100

    Added underline below navlinks

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -20,13 +20,13 @@ export default function Navbar() {
         </Link>
         
         <div className="flex gap-2 text-sm md:text-base md:gap-6">
-          <a onClick={() => handleScroll('menu')} className="hover:text-gray-600 scroll-smooth">
+          <a onClick={() => handleScroll('menu')} className="hover:text-gray-600 relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 hover:after:w-full after:bg-gray-600 after:transition-all after:duration-300 scroll-smooth">
             Menu
           </a>
-          <a onClick={() => handleScroll('bordbestilling')} className="hover:text-gray-600 scroll-smooth">
+          <a onClick={() => handleScroll('bordbestilling')} className="hover:text-gray-600 relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 hover:after:w-full after:bg-gray-600 after:transition-all after:duration-300 scroll-smooth">
             Bordbestilling
           </a>
-          <a onClick={() => handleScroll('findus')} className="hover:text-gray-600 scroll-smooth">
+          <a onClick={() => handleScroll('findus')} className="hover:text-gray-600 relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 hover:after:w-full after:bg-gray-600 after:transition-all after:duration-300 scroll-smooth">
             Find os
           </a>
         </div>
